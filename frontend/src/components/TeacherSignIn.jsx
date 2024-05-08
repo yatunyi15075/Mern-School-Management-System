a70c7640fd182838948322db73a102b776c4ae9b
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-08 14:34:50 -0700

    Update TeacherSignIn.jsx

--- frontend/src/components/TeacherSignIn.jsx
+++ frontend/src/components/TeacherSignIn.jsx
@@ -6,6 +6,8 @@ const TeacherSignInContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  background: linear-gradient(45deg, #FF69B4, #FFA07A, #90EE90); /* Gradient background */
+  height: 100vh; /* Full height of the viewport */
 `;
 
 const FormContainer = styled.form`
@@ -30,17 +32,23 @@ const InputField = styled.input`
 
 const SubmitButton = styled.button`
   width: 100%;
-  padding: 10px;
+  padding: 12px;
   margin-top: 20px;
   border: none;
-  border-radius: 4px;
-  background-color: #007bff;
+  border-radius: 8px;
+  background-color: #FF4500;
   color: white;
-  font-size: 16px;
+  font-size: 18px;
+  text-decoration: none;
+  text-align: center;
   cursor: pointer;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: #FF6347;
+  }
 `;
 
-const AdminSignIn = () => {
+const TeacherSignInn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -69,12 +77,12 @@ const AdminSignIn = () => {
           required
         /> 
         {/* Use Link component to navigate to admin dashboard */}
-        <Link to="/teacher/dashboard">
-          <SubmitButton onClick={handleSignIn}>Sign In</SubmitButton>
-        </Link>
+
+          <SubmitButton to="/teacher/dashboard" onClick={handleSignIn}>Sign In</SubmitButton>
+
       </FormContainer>
     </TeacherSignInContainer>
   );
 };
 
-export default AdminSignIn;
+export default TeacherSignIn;
