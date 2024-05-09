83709cd2a2ace2c39333c7b9b92c33176951c259
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-09 06:30:26 -0700

    Update assignmentController.js

--- backend/controllers/assignmentController.js
+++ backend/controllers/assignmentController.js
@@ -1,23 +1,33 @@
+// assignmentController.js
+
 import { Assignment } from "../models/assignmentSchema.js";
+import { handleValidationError } from "../middlewares/errorHandler.js";
 
 export const createAssignment = async (req, res, next) => {
+  console.log(req.body);
   const { title, description, grade, deadline } = req.body;
-  if (!title || !description || !grade || !deadline ) {
-    return next("Please Fill Full Form!", 400);
+  try {
+    if (!title || !description || !grade || !deadline) {
+      handleValidationError("Please Fill Full Form!", 400);
+    }
+    await Assignment.create({ title, description, grade, deadline });
+    res.status(201).json({
+      success: true,
+      message: "Assignment Created!",
+    });
+  } catch (err) {
+    next(err);
+  } 
+};
+
+export const getAllAssignments = async (req, res, next) => {
+  try {
+    const assignments = await Assignment.find();
+    res.status(200).json({
+      success: true,
+      assignments,
+    });
+  } catch (err) {
+    next(err);
   }
-  await Assignment.create({ title, description, grade, deadline  });
-  res.status(200).json({
-    success: true,
-    message: "Assignment Created!",
-  });
-};
-
-export const getAllAssignments= async (req, res, next) => {
-  const assignments = await Assignment.find();
-  res.status(200).json({
-    success: true,
-    assignments,
-  });
-};
-
-
+}; 
