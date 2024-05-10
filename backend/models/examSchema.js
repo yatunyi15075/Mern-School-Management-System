8b3bc387c681b45e526b3c7a554b92e7fcec995f
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-10 03:18:28 -0700

    Update examSchema.js

--- backend/models/examSchema.js
+++ backend/models/examSchema.js
@@ -1,1 +1,23 @@
+import mongoose from "mongoose";
+
+const examSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  registrationNumber: {
+    type: String,
+    required: true
+  },
+  className: {
+    type: String,
+    required: true
+  },
+  marks: {
+    type: Number,
+    required: true
+  }
+});
+
+export const Exam = mongoose.model('Exam', examSchema);
 
