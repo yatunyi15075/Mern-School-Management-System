86d6c3e8842f5fe99e727f3c5b5bb922fff5133f
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-09 23:11:10 -0700

    Update announcementConroller.js

--- backend/controllers/announcementConroller.js
+++ backend/controllers/announcementConroller.js
@@ -1,23 +1,33 @@
 import {Announcement} from "../models/announcementSchema.js";
+import { handleValidationError } from "../middlewares/errorHandler.js";
 
 export const createAnnouncement = async (req, res, next) => {
+  console.log(req.body);
   const { announcement } = req.body;
-  if (!announcement ) {
-    return next("Please Fill Form!", 400);
+  try {
+      if (!announcement ) {
+        handleValidationError("Please Fill Form!", 400);
   }
   await Announcement.create({ announcement});
   res.status(200).json({
     success: true,
     message: "Announcement Created!",
   });
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const getAllAnnouncements = async (req, res, next) => {
-  const announcements = await Announcement.find();
-  res.status(200).json({
+  try {
+    const announcements = await Announcement.find();
+    res.status(200).json({
     success: true,
     announcements,
-  });
+  }); 
+  } catch (err) {
+    next(err);
+  }
 };
 
 
