bd7886dd8decdb06ac067b3457e512ce6230a32c
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-10 03:18:55 -0700

    Update examRouter.js

--- backend/router/examRouter.js
+++ backend/router/examRouter.js
@@ -1,1 +1,10 @@
+import express from "express";
+import { getAllExams, addExam } from "../controllers/examController.js";
 
+const router = express.Router();
+
+router.get('/getall', getAllExams);
+router.post('/', addExam);
+
+
+export default router; 
