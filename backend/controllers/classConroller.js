449aeebe522e0df7695dbe44d15aa8fc7c062b05
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-09 23:11:51 -0700

    Update classConroller.js

--- backend/controllers/classConroller.js
+++ backend/controllers/classConroller.js
@@ -1,24 +1,32 @@
 import {  Class } from "../models/classSchema.js";
+import { handleValidationError } from "../middlewares/errorHandler.js";
 
 export const createClass = async (req, res, next) => {
+  console.log(req.body);
   const {grade } = req.body;
-  if (!grade ) {
-    return next("Please Fill Form!", 400);
+  try {
+    if (!grade ) {
+      handleValidationError("Please Fill Form!", 400);
   }
   await Class.create({ grade });
   res.status(200).json({
     success: true,
     message: "Class Created!",
-  });
+  }); 
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const getAllClasses = async (req, res, next) => {
+  try {
   const classes = await Class.find();
   res.status(200).json({
     success: true,
     classes,
-  });
+  });  
+  } catch (err) {
+    next(err);
+  }
 };
-
-
-
+ 
