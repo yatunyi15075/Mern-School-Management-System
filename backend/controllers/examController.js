dd3e70dbd4781127dccd1bf3a6b9acc5e6189e21
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-10 03:17:55 -0700

    Update examController.js

--- backend/controllers/examController.js
+++ backend/controllers/examController.js
@@ -1,1 +1,32 @@
 
+import {Exam} from "../models/examSchema.js";
+import { handleValidationError } from "../middlewares/errorHandler.js";
+
+export const addExam = async (req, res, next) => {
+  console.log(req.body);
+  const { name, registrationNumber, className, marks } = req.body;
+  try {
+    if (!name || !registrationNumber || !className || !marks) {
+        handleValidationError("Please fill out all fields!", 400);
+    }
+    await Exam.create({ name, registrationNumber, className, marks });
+    res.status(200).json({
+      success: true,
+      message: "A new exam has been added!",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getAllExams = async (req, res, next) => {
+  try {
+    const exams = await Exam.find();
+    res.status(200).json({
+      success: true,
+      exams,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
