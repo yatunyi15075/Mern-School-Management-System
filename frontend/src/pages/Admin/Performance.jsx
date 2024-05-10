6480c19ea354f7bb183327e7d1a9316d061126ec
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-10 00:14:34 -0700

    Update Performance.jsx

--- frontend/src/pages/Admin/Performance.jsx
+++ frontend/src/pages/Admin/Performance.jsx
@@ -4,6 +4,7 @@ import Sidebar from './Sidebar'; // Import the Sidebar component
 
 const PerformanceContainer = styled.div`
   display: flex;
+  padding-left: 240px;
 `;
 
 const Content = styled.div`
