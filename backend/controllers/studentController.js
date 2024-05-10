f8f10779eee0b41a5acc10074751a62194cbb42d
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-09 23:14:21 -0700

    Update studentController.js

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -1,24 +1,33 @@
 import { Student } from "../models/studentSchema.js";
+import { handleValidationError } from "../middlewares/errorHandler.js";
 
 export const createStudent = async (req, res, next) => {
   console.log(req.body);
   const { name, registrationNumber, grade } = req.body;
-  if (!name || !registrationNumber || !grade ) {
+  try {
+   if (!name || !registrationNumber || !grade ) {
     return next("Please Fill Full Form!", 400);
   }
   await Student.create({ name, registrationNumber, grade });
   res.status(200).json({
     success: true,
     message: "Student Created!",
-  });
+  });   
+} catch (err) {
+  next(err);
+} 
 };
 
 export const getAllStudents = async (req, res, next) => {
-  const students = await Student.find();
+  try {
+   const students = await Student.find();
   res.status(200).json({
     success: true,
     students,
-  });
+  });   
+} catch (err) {
+  next(err);
+}
 };
 
 
