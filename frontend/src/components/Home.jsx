1dee3d59afb20de23cc1a9ecf304fafc54442b1e
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-11 12:48:08 -0700

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,132 +1,13 @@
+// Home.js
 import React from 'react';
-import styled from 'styled-components';
+import { Navbar, Logo, NavigationLinks, NavLink, ButtonsContainer, LoginButton, GuestButton, HomeContainer, SchoolInfo, SchoolImage, Title, LoremTextContainer, AdminRegisterLink } 
+from '../styles/styles'
 import { LoremIpsum } from 'lorem-ipsum';
 import bg from "../assets/bg.png";
 import bg1 from "../assets/bg1.png";
-import hero from "../assets/hero.jpg";
 import { Link, useNavigate } from 'react-router-dom'; 
 
 const lorem = new LoremIpsum();
-
-const Navbar = styled.nav`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 20px;
-  background-color: #6BD4E7; /* Eye-catching teal background color */
-  color: black;
-  font-family: Arial, sans-serif;
-  z-index: 1000; /* Ensure the navbar stays above other content */
-`;
-
-const Logo = styled.img`
-  width: 50px;
-  height: auto;
-`;
-
-const NavigationLinks = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const NavLink = styled.a`
-  margin-right: 20px;
-  color: black;
-  text-decoration: none;
-  font-size: 18px;
-  font-weight: bold;
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
-const ButtonsContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-right: 35px; /* Add margin to separate buttons from the edge */
-`;
-
-const LoginButton = styled.button`
-  background-color: orange;
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  margin-right: 10px;
-  cursor: pointer;
-  font-size: 16px;
-  font-weight: bold;
-`;
-
-const GuestButton = styled.button`
-  color: white;
-  border: none;
-  background-color: transparent;
-  padding: 10px 20px;
-  cursor: pointer;
-  font-size: 16px;
-  font-weight: bold;
-  border: 2px solid orange;
-  border-radius: 5px;
-  background-color: transparent;
-  transition: background-color 0.3s ease;
-  &:hover {
-    background-color: orange;
-  }
-`;
-
-const HomeContainer = styled.div`
-  display: flex;
-  flex-direction: column; 
-  align-items: center;
-  text-align: center;
-  background: linear-gradient(45deg, #6BD4E7, #6FC3DF); /* Gradient background */
-  background-size: cover;
-  background-position: center;
-  min-height: 100vh;
-  padding-top: 80px; /* Add padding-top to push content below the fixed navbar */
-`;
-
-const SchoolInfo = styled.div`
-  margin-top: 20px;
-`;
-
-const SchoolImage = styled.img`
-  width: 80%;
-  max-height: 80vh;
-  object-fit: cover;
-  margin-top: 20px;
-`;
-
-const Title = styled.h1`
-  font-size: 36px;
-  font-weight: bold;
-  color: white;
-  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
-`;
-
-const LoremTextContainer = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
-  font-size: 18px;
-  color: white;
-  text-align: justify;
-  padding: 0 20px;
-`;
-
-const AdminRegisterLink = styled(Link)`
-  color: white;
-  font-size: 12px;
-  font-weight: bold;
-  text-decoration: none;
-  margin-top: 10px;
-  &:hover {
-    text-decoration: underline;
-  }
-`;
 
 const Home = () => {
   const navigate = useNavigate();
