4dac32442d64d74cad01fa4746549a3aedf206a0
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-11 12:57:47 -0700

    Update Dashboard.jsx

--- frontend/src/pages/Students/Dashboard.jsx
+++ frontend/src/pages/Students/Dashboard.jsx
@@ -1,48 +1,8 @@
+// StudentDashboard.js
 import React from 'react';
-import styled from 'styled-components';
 import Sidebar from './Sidebar';
-
-const StudentDashboardContainer = styled.div`
-  display: flex;
-  padding-left: 240px;
-`;
-
-const Content = styled.div`
-  flex: 1;
-  padding: 20px;
-`;
-
-const Section = styled.section`
-  margin-bottom: 40px;
-`;
-
-const SectionTitle = styled.h2`
-  font-size: 24px;
-  margin-bottom: 20px;
-`;
-
-const CardContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  grid-gap: 20px;
-`;
-
-const Card = styled.div`
-  background-color: #ffffff;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-`;
-
-const CardTitle = styled.h3`
-  font-size: 18px;
-  margin-bottom: 10px;
-`;
-
-const CardContent = styled.p`
-  font-size: 16px;
-  color: #555555;
-`;
+import { StudentDashboardContainer, Content, Section, SectionTitle, CardContainer, Card, CardTitle, CardContent } 
+from '../../styles/DashboardStyles';
 
 const StudentDashboard = () => {
   return (
