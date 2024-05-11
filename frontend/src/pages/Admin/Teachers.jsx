ebad228fac14c004e8e2bab3092a2007a394a297
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-11 12:55:51 -0700

    Update Teachers.jsx

--- frontend/src/pages/Admin/Teachers.jsx
+++ frontend/src/pages/Admin/Teachers.jsx
@@ -1,58 +1,18 @@
+// Teachers.js
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import Sidebar from './Sidebar'; // Import the Sidebar component
+import Sidebar from './Sidebar';
 import axios from 'axios';
-
-const TeachersContainer = styled.div`
-  display: flex;
-  padding-left: 240px;
-`;
-
-const Content = styled.div`
-  flex: 1;
-`;
-
-const TeachersContent = styled.div`
-  padding: 20px;
-`;
-
-const TeachersHeader = styled.h2`
-  font-size: 24px;
-  margin-bottom: 20px;
-`;
-
-const TeacherList = styled.ul`
-  list-style: none;
-  padding: 0;
-`;
-
-const TeacherItem = styled.li`
-  background-color: #f9f9f9;
-  border-radius: 8px;
-  padding: 10px 20px;
-  margin-bottom: 10px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`;
-
-const AddTeacherForm = styled.form`
-  margin-bottom: 20px;
-`;
-
-const AddTeacherInput = styled.input`
-  padding: 8px;
-  margin-right: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-`;
-
-const AddTeacherButton = styled.button`
-  padding: 8px 16px;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-`;
+import {
+  TeachersContainer,
+  Content,
+  TeachersContent,
+  TeachersHeader,
+  TeacherList,
+  TeacherItem,
+  AddTeacherForm,
+  AddTeacherInput,
+  AddTeacherButton,
+} from '../../styles/TeachersStyles'; // Import styled components from TeachersStyles.js
 
 const Teachers = () => {
   const [newTeacher, setNewTeacher] = useState({ name: '', email: '', subject: '' });
@@ -87,7 +47,7 @@ const Teachers = () => {
 
   return (
     <TeachersContainer>
-      <Sidebar /> {/* Include the Sidebar component */}
+      <Sidebar />
       <Content>
         <TeachersContent>
           <TeachersHeader>Teachers</TeachersHeader>
