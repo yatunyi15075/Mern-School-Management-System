40155cf9a36a49c8a3bd339232dbbd07f5ea4f84
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-11 12:53:01 -0700

    Update EventCalender.jsx

--- frontend/src/pages/Admin/EventCalender.jsx
+++ frontend/src/pages/Admin/EventCalender.jsx
@@ -1,63 +1,18 @@
+// EventCalendar.js
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import Sidebar from './Sidebar';
 import axios from 'axios';
-
-const EventCalendarContainer = styled.div`
-  display: flex;
-  padding-left: 240px;
-`;
-
-const Content = styled.div`
-  flex: 1;
-  padding: 20px;
-`;
-
-const CalendarContainer = styled.div`
-  margin-top: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  padding: 20px;
-`;
-
-const Events = styled.div`
-  margin-top: 20px;
-`;
-
-const Event = styled.div`
-  margin-bottom: 10px;
-  padding: 10px;
-  background-color: #f9f9f9;
-  border-radius: 5px;
-`;
-
-const AddEventForm = styled.form`
-  margin-top: 20px;
-  display: flex;
-  align-items: center;
-`;
-
-const EventInput = styled.input`
-  flex: 1;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  margin-right: 10px;
-`;
-
-const AddEventButton = styled.button`
-  padding: 10px 20px;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-`;
-
-const ErrorText = styled.p`
-  color: red;
-  margin-top: 10px;
-`;
+import {
+  EventCalendarContainer,
+  Content,
+  CalendarContainer,
+  Events,
+  Event,
+  AddEventForm,
+  EventInput,
+  AddEventButton,
+  ErrorText,
+} from '../../styles/EventCalendarStyles';
 
 const EventCalendar = () => {
   const [events, setEvents] = useState([]);
