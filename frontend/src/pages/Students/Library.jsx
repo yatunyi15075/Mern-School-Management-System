81489f0ee477e29fe8c5b6e8ba732855b48f1c02
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-11 12:58:34 -0700

    Update Library.jsx

--- frontend/src/pages/Students/Library.jsx
+++ frontend/src/pages/Students/Library.jsx
@@ -1,51 +1,17 @@
+// LibrarySection.js
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import Sidebar from './Sidebar';
 import axios from 'axios';
-
-const LibraryContainer = styled.div`
-  display: flex;
-`;
-
-const SidebarContainer = styled.div`
-  flex: 0 0 250px; /* Sidebar width */
-`;
-
-const Content = styled.div`
-  flex: 1;
-  padding: 20px;
-`;
-
-const LibraryHeader = styled.h1`
-  font-size: 24px;
-  margin-bottom: 20px;
-`;
-
-const BookList = styled.ul`
-  list-style: none;
-  padding: 0;
-`;
-
-const BookItem = styled.li`
-  background-color: #fff;
-  border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  padding: 20px;
-  margin-bottom: 20px;
-`;
-
-const BookTitle = styled.h3`
-  margin-bottom: 10px;
-`;
-
-const BorrowButton = styled.button`
-  padding: 8px 16px;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-`;
+import {
+  LibraryContainer,
+  SidebarContainer,
+  Content,
+  LibraryHeader,
+  BookList,
+  BookItem,
+  BookTitle,
+  BorrowButton,
+} from '../../styles/LibraryStyles';
 
 const LibrarySection = () => {
   const [books, setBooks] = useState([]);
