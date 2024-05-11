950ccf1450295e0f024cdea928ee98b8be740f16
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-11 12:52:07 -0700

    Update Classes.jsx

--- frontend/src/pages/Admin/Classes.jsx
+++ frontend/src/pages/Admin/Classes.jsx
@@ -1,58 +1,18 @@
+// Classes.js
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import Sidebar from './Sidebar'; // Import the Sidebar component
+import Sidebar from './Sidebar';
 import axios from 'axios';
-
-const ClassesContainer = styled.div`
-  display: flex;
-  padding-left: 240px;
-`;
-
-const Content = styled.div`
-  flex: 1;
-`;
-
-const ClassesContent = styled.div`
-  padding: 20px;
-`;
-
-const ClassesHeader = styled.h2`
-  font-size: 24px;
-  margin-bottom: 20px;
-`;
-
-const ClassList = styled.ul`
-  list-style: none;
-  padding: 0;
-`;
-
-const ClassItem = styled.li`
-  background-color: #f9f9f9;
-  border-radius: 8px;
-  padding: 10px 20px;
-  margin-bottom: 10px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`;
-
-const AddClassForm = styled.form`
-  margin-bottom: 20px;
-`;
-
-const AddClassInput = styled.input`
-  padding: 8px;
-  margin-right: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-`;
-
-const AddClassButton = styled.button`
-  padding: 8px 16px;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-`;
+import {
+  ClassesContainer,
+  Content,
+  ClassesContent,
+  ClassesHeader,
+  ClassList,
+  ClassItem,
+  AddClassForm,
+  AddClassInput,
+  AddClassButton,
+} from '../../styles/ClassesStyles';
 
 const Classes = () => {
   const [newClassName, setNewClassName] = useState('');
@@ -74,8 +34,6 @@ const Classes = () => {
       console.error('Error fetching classes:', error.message);
     }
   };
-  
-  
 
   const handleAddClass = async (e) => {
     e.preventDefault();
@@ -100,7 +58,7 @@ const Classes = () => {
 
   return (
     <ClassesContainer>
-      <Sidebar /> 
+      <Sidebar />
       <Content>
         <ClassesContent>
           <ClassesHeader>Classes</ClassesHeader>
@@ -114,12 +72,11 @@ const Classes = () => {
             <AddClassButton type="submit">Add Class</AddClassButton>
           </AddClassForm>
           <ClassList>
-  {/* Ensure that classes is an array before mapping over it */}
-  {Array.isArray(classes) && classes.map((classItem, index) => (
-    <ClassItem key={index}>{classItem.grade}</ClassItem> 
-  ))}
-</ClassList>
-
+            {/* Ensure that classes is an array before mapping over it */}
+            {Array.isArray(classes) && classes.map((classItem, index) => (
+              <ClassItem key={index}>{classItem.grade}</ClassItem>
+            ))}
+          </ClassList>
         </ClassesContent>
       </Content>
     </ClassesContainer>
