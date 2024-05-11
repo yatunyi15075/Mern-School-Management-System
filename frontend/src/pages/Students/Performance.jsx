aa25b13747ab7e08081331038ac05bc8b9449d90
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-11 12:59:02 -0700

    Update Performance.jsx

--- frontend/src/pages/Students/Performance.jsx
+++ frontend/src/pages/Students/Performance.jsx
@@ -1,40 +1,16 @@
+// PerformanceSection.js
 import React from 'react';
-import styled from 'styled-components';
 import Sidebar from './Sidebar';
-import { Line } from 'react-chartjs-2';
-
-const PerformanceContainer = styled.div`
-  display: flex;
-`;
-
-const SidebarContainer = styled.div`
-  flex: 0 0 250px; /* Sidebar width */
-`;
-
-const Content = styled.div`
-  flex: 1;
-  padding: 20px;
-`;
-
-const PerformanceHeader = styled.h1`
-  font-size: 24px;
-  margin-bottom: 20px;
-`;
-
-const PerformanceInfo = styled.div`
-  background-color: #fff;
-  border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  padding: 20px;
-`;
-
-const PerformanceGraphContainer = styled.div`
-  margin-bottom: 20px;
-`;
-
-const TotalMarks = styled.div`
-  font-weight: bold;
-`;
+// import { Line } from 'react-chartjs-2';
+import {
+  PerformanceContainer,
+  SidebarContainer,
+  Content,
+  PerformanceHeader,
+  PerformanceInfo,
+  PerformanceGraphContainer,
+  TotalMarks,
+} from '../../styles/PerformanceStyles'; // Import styled components from PerformanceSectionStyles.js
 
 const PerformanceSection = () => {
   // Sample performance data
