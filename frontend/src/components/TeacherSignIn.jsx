1ff9cb5bb97832b6f663eeecca5bb25b6fe1888f
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-11 12:48:57 -0700

    Update TeacherSignIn.jsx

--- frontend/src/components/TeacherSignIn.jsx
+++ frontend/src/components/TeacherSignIn.jsx
@@ -1,59 +1,13 @@
+// TeacherSignIn.js
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { TeacherSignInContainer, FormContainer, InputField, SubmitButton } from '../styles/TeacherSignInStyles';
 
-const TeacherSignInContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background: linear-gradient(45deg, #FF69B4, #FFA07A, #90EE90); /* Gradient background */
-  height: 100vh; /* Full height of the viewport */
-`;
-
-const FormContainer = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 300px;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  background-color: #f9f9f9;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`;
-
-const InputField = styled.input`
-  width: 100%;
-  padding: 10px;
-  margin: 10px 0;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-`;
-
-const SubmitButton = styled.button`
-  width: 100%;
-  padding: 12px;
-  margin-top: 20px;
-  border: none;
-  border-radius: 8px;
-  background-color: #FF4500;
-  color: white;
-  font-size: 18px;
-  text-decoration: none;
-  text-align: center;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-  &:hover {
-    background-color: #FF6347;
-  }
-`;
-
-const TeacherSignInn = () => {
+const TeacherSignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSignIn = () => {
-    // For demonstration purposes, we'll directly navigate to the admin dashboard route
+    // For demonstration purposes, we'll directly navigate to the teacher dashboard route
     // Replace this with your actual sign-in logic
     console.log('Teacher Sign In:', { email, password });
   };
@@ -76,10 +30,8 @@ const TeacherSignInn = () => {
           onChange={(e) => setPassword(e.target.value)}
           required
         /> 
-        {/* Use Link component to navigate to admin dashboard */}
-
-          <SubmitButton to="/teacher/dashboard" onClick={handleSignIn}>Sign In</SubmitButton>
-
+        {/* Use Link component to navigate to teacher dashboard */}
+        <SubmitButton to="/teacher/dashboard" onClick={handleSignIn}>Sign In</SubmitButton>
       </FormContainer>
     </TeacherSignInContainer>
   );
