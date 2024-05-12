5b340207e3ad63978b86ea437a46054b10399fe5
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-11 23:51:42 -0700

    Update AdminRegister.jsx

--- frontend/src/components/AdminRegister.jsx
+++ frontend/src/components/AdminRegister.jsx
@@ -1,15 +1,28 @@
-// AdminRegister.js
 import React, { useState } from 'react';
 import { AdminRegisterContainer, FormContainer, InputField, SubmitButton } from '../styles/AdminRegisterStyles';
+import axios from 'axios'; // Import axios
 
 const AdminRegister = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleRegister = () => {
-    // For demonstration purposes, we'll simply log the registration data
-    console.log('Admin Registration:', { email, password });
+  const handleRegister = async (e) => {
+    e.preventDefault(); // Prevent default form submission
+  
+    try {
+      const response = await axios.post('http://localhost:4000/api/v1/register/admin', { email, password }); 
+      if (response.status === 200) {
+        // Registration successful, redirect to admin login
+        window.location.href = '/admin-signIn';
+      } else {
+        // Handle registration errors
+        console.error('Registration failed');
+      }
+    } catch (error) {
+      console.error('Error during registration:', error);
+    }
   };
+   
 
   return (
     <AdminRegisterContainer>
@@ -29,7 +42,8 @@ const AdminRegister = () => {
           onChange={(e) => setPassword(e.target.value)}
           required
         />
-        <SubmitButton type="button" onClick={handleRegister}>Register</SubmitButton>
+        <SubmitButton onClick={(e) => handleRegister(e)}>Register</SubmitButton>
+
       </FormContainer>
     </AdminRegisterContainer>
   );
