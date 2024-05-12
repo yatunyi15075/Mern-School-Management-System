1229921d1b29a89d08fa52755cb394cabbd1cc67
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-12 00:10:08 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,18 +10,29 @@ import announcementRouter from "./router/announcementRouter.js";
 import classRouter from "./router/classRouter.js";
 import libraryRouter from "./router/libraryRouter.js";
 import eventsRouter from "./router/eventsRouter.js";
+import examRouter from "./router/examRouter.js";
+import attendanceRouter from "./router/attendanceRouter.js";
+import usersRouter from "./router/usersRouter.js"
+import adminRegisterRouter from "./router/adminRegisterRouter.js"
+import  { errorHandler } from "./middlewares/errorHandler.js";
+
+
 
 const app = express();
 config({path: "./config/config.env"});
-
+ 
 app.use( 
     cors({
         origin: [process.env.FRONTEND_URL],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true,
-    })
+        methods: ["GET", "POST", "PUT", "DELETE"], 
+    
+    }) 
 );
 
+app.use((err, req, res, next) => {
+    errorHandler(err, req, res, next);
+  });
+ 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -33,7 +44,12 @@ app.use("/api/v1/announcements", announcementRouter);
 app.use("/api/v1/class", classRouter);
 app.use("/api/v1/library", libraryRouter);
 app.use("/api/v1/events", eventsRouter);
+app.use("/api/v1/exam", examRouter);
+app.use("/api/v1/attendance", attendanceRouter);
+
+app.use("/api/v1/users", usersRouter);
+app.use("/api/v1/register", adminRegisterRouter);
 
 dbConnection()
-
+ 
 export default app;
